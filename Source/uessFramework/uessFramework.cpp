4f7e22f8fd4a4a031a048e7d1cd93c35e9030ea0
Louis Stockdale-Lees <lslees1346@gmail.com>
2025-11-20 00:17:43 +0000

    spawning

---
+++ Source/uessFramework/uessFramework.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "uessFramework.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, uessFramework, "uessFramework" );
